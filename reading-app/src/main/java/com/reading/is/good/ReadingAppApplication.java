ed41ffea260d319164e2dfd2a442e61f03aee464
ariffkilic <ariffkilic@gmail.com>
2021-06-01 16:39:39 +0300

    swagger integration

--- reading-app/src/main/java/com/reading/is/good/ReadingAppApplication.java
+++ reading-app/src/main/java/com/reading/is/good/ReadingAppApplication.java
@@ -2,12 +2,27 @@ package com.reading.is.good;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ReadingAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReadingAppApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.reading.is.good.service.api"))
+				.paths(PathSelectors.regex("/api.*")).build();
 	}
 
 }
